61c6da94642604b9470783e680b62839380579d5
hyeonjiwon <zlone1208@gmail.com>
2023-03-16 13:35:43 +0900

    구조분해할당

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 
 /*
 - 구체적인 앱은 app.js에서 
@@ -9,15 +10,29 @@ import './App.css';
 - 컴포넌트는 반드시 대문자로 정의한다. -> native 태그와 구분 할 수 있음
 - 컴포넌트의 실체는 함수이고, 함수가 return 값이 ui가 된다.  
 - img 태그는 src 속성에 따라 변경 
-- 
+- 배포(vercel) > npm run build > npx serve -s build
+- 구조분해할당
+- props는 컴포넌트의 입력값이다.
+- 이벤트 안에는 함수가 온다.
+- 문자열이 아닌 데이터 타입은 {}로 묶는다.
 */
 
-function Counter(props) {
-  console.log('props', props);
+function Counter({title, initValue}) { // props
+  // let countState = useState(initValue);
+  // let count = countState[0]; 
+  // let setCount = countState[1]; // 값을 수정할 때 사용
+
+  // console.log(countState);
+
+  const  [count, setCount] = useState(initValue); // 위의 코드와 동일한 의미
+
   return (
     <div>
-      <h1>{props.title}</h1>
-      <button>🎀</button> 👉 {props.initValue}
+      <h1>{title}</h1>
+      <button onClick={function(){
+        console.log("up");
+        setCount(count + 1);
+      }}>🎀</button> 👉👉 {count}
     </div>
   );
 }
@@ -25,8 +40,8 @@ function Counter(props) {
 function App() {
   return (
     <div>
-      <Counter title = "불면증카운터" initValue = "10"></Counter>
-      <Counter title = "입장객카운터" initValue = "20"></Counter>
+      <Counter title = "불면증카운터" initValue = {10}></Counter> 
+      <Counter title = "입장객카운터" initValue = {20}></Counter> 
       <img src="/img.jpg"></img>
     </div>
   );
